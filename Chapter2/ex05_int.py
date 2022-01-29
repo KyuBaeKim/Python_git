e33f206efd91d4c90721a9ade4b277e7ed1b62c0
kyubae <kkyub5118@gmail.com>
2022-01-29 15:19:36 +0900

    int

---
+++ Chapter2/ex05_int.py
@@ -0,0 +1,17 @@
+from socket import SocketIO
+
+
+price = input('가격을 입력해 주세요 : ')
+num = input('수량을 입력해 주세요 : ')
+sum = int(price) * int(num)
+print('총액은', sum, '원 입니다', sep='')
+
+# int () 정수 변환 함수
+
+print("")
+
+# 변수 선언
+score = 98
+print(score)
+score = '안녕하세요'
+print(score)
