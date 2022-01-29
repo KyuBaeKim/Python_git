a8f2c75160e4c9394023a4b8c22745b5e94c1613
kyubae <kkyub5118@gmail.com>
2022-01-29 15:26:46 +0900

    float_complex

---
+++ Chapter3/ex02_float.py
@@ -0,0 +1,13 @@
+
+from sklearn.metrics import jaccard_score
+
+# 실수
+a = 12345.456
+print(a)
+print(type(a))
+
+# 복소수 --> a+bj ( a = 실수부, b = 허수부)
+a = 1 + 2j
+b = 3 + 4j
+print(a+b)
+print(type(a+b))
