d9900975546b0a13b3f684a12694296b374a08ec
kyubae <kkyub5118@gmail.com>
2022-01-29 15:47:26 +0900

    collection

---
+++ Chapter3/ex09_list_tuple.py
@@ -0,0 +1,22 @@
+# 컬렉션 -> list, tuple
+
+# list
+member = ['가', '나', '다', '라'] # --> list
+# 리스트는 기존의 값을 수정 변경이 가능하다
+
+print(type(member))
+print((member))
+print((member[0]))
+print((member[1]))
+print((member[2]))
+print((member[3]))
+
+# tuple
+member = ('가','나','다','라')
+# 튜플은 값을 변경할수없다.
+print(type(member))
+print((member))
+print((member[0]))
+print((member[1]))
+print((member[2]))
+print((member[3]))
