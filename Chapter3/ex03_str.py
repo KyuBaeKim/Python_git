3e4e6aad6324fe4a3cdff8330d21efc5110e352b
kyubae <kkyub5118@gmail.com>
2022-01-29 15:29:55 +0900

    str

---
+++ Chapter3/ex03_str.py
@@ -0,0 +1,15 @@
+# 문자열 타입
+a = 'hello'
+
+b = 'bye'
+
+c = """ 
+HELLO
+BYE
+""" # 3중 따움표는 줄바꿔도 사용가능
+# "HELLO "BYE" HELLO " 잘못된 표현의 따옴표
+
+# "HELLO 'BYE' HELLO " 맞는표현
+print("HELLO \"BYE\" HELLO ")
+# 역 슬래시 이용해서 사용가능
+print("HELLO 'BYE' HELLO ")
