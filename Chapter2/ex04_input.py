a8f9f23dc7f49f83f63d7a222dfec2050ed77f06
kyubae <kkyub5118@gmail.com>
2022-01-29 15:16:52 +0900

    Input

---
+++ Chapter2/ex04_input.py
@@ -0,0 +1,9 @@
+# input 선언
+age = input("몇 살이에요?") # input을 통해 키보드로 입력하면 문자열로 인식
+print(age)
+
+# print(age*2) -> 입력 29  출력 2929
+# print(age+2) -> 입력 29  출력 error (input 값은 문자열이기 때문에)
+
+# 정수 -> int
+# 문자열 -> str
