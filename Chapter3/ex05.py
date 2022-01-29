7f93f1cdfa343dcf6b3d52769651addeaa6d5307
kyubae <kkyub5118@gmail.com>
2022-01-29 15:37:18 +0900

    문자열_결합

---
+++ Chapter3/ex05.py
@@ -0,0 +1,19 @@
+value = 12 * 24 * 45* 89
+
+# 연산을 다음줄에 이어서 표기 할 경우
+
+value = 12 * 45 * \
+    45 * 89
+    
+value = (12 * 45 *
+         45 * 89)
+
+# 문자열 결합 방법
+s = "hi" + "hello" + "my" + "name"
+print(s)
+
+s = ("hi"
+     "hello"
+     "my"
+     "name")
+print(s)
