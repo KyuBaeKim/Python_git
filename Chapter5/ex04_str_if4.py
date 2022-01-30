6b7c39ef8353a00c237871a6a7464adb15efe4d7
kyubae <kkyub5118@gmail.com>
2022-01-30 15:41:19 +0900

    문자열 비교

---
+++ Chapter5/ex04_str_if4.py
@@ -0,0 +1,9 @@
+# 문자열 비교
+# 문자열은 각 아스키 코드 값을 이용하여 크기를 비교한다.
+
+if 'Korea' > 'Japan':
+    print("Korea가 더 크다.")
+if 'Korea' < 'Japan' :
+    print("Japan이 더크다.")
+if 'KoRea' < 'Japan' :
+    print("------------------")
