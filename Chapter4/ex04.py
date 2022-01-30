9f0849d553ecfaa71bfb389816730cfba237a177
kyubae <kkyub5118@gmail.com>
2022-01-30 15:24:23 +0900

    str + bool

--- Chapter4/ex04.py
+++ Chapter4/ex04.py
@@ -9,3 +9,14 @@ print(s1+s2)
 # 숫자를 문자열로 바꾸는법
 print(s1+ '10000')
 print(s1+ str(10000))
+
+print()
+
+# *
+print('='*40)
+print('최고' * 3)
+print('='*40)
+
+print('\n','='*30,sep = '')
+print('KIM KYU BAE')
+print('='*30)
---
+++ Chapter4/ex05.py
@@ -0,0 +1,7 @@
+# 숫자를 문자열로 바꾸는 방법
+s = 'korea' + str(2002)
+print(s)
+print(type(s)) # ---> str의 변수 유형을 알려주낟.
+
+print(bool(1)) # ---> 1이 bool에서 쓰이는 뜻이 무엇인지 출력한다.
+print(bool(0))
