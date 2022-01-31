6af94cd6f16ae212b579c2bcd60be0fee80c8108
kyubae <kkyub5118@gmail.com>
2022-01-31 14:15:23 +0900

    2중 배열

---
+++ Chapter6/ex12.py
@@ -0,0 +1,11 @@
+for y in range(1, 10):
+    for x in range(y) :
+        print('*', end = '')
+    print()
+    
+print("="*30)
+
+for y in range(9, 0, -1):
+    for x in range(y):
+        print('*', end='')
+    print()
