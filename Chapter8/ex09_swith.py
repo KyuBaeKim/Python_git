ed7c22447b28384d44081dbd466b22d01fd12044
kyubae <kkyub5118@gmail.com>
2022-02-02 13:03:38 +0900

    swith

---
+++ Chapter8/ex09_swith.py
@@ -0,0 +1,14 @@
+# swith
+# starswith, endswith
+name = '홍길동'
+
+if name.startswith("홍"):
+    print("홍씨입니다.")
+
+if name.startswith("김"):
+    print("김씨입니다.")
+
+file = "figule.jpg"    
+if file.endswith(".jpg"):
+    print(".jpg 그림 파일입니다.")
+    
