48a8893eb99ca5e3b243628cf848022b653708e1
kyubae <kkyub5118@gmail.com>
2022-02-02 15:54:20 +0900

    dic_print

---
+++ Chapter10/ex02_dic_print.py
@@ -0,0 +1,9 @@
+# dic의 인덱싱은 dic['키]형태로 반환하면 된다.
+
+def main():
+    dic = {  'boy': '소년',  'school': '학교', 'book': '책' }
+
+    print(dic['boy'])
+    print(dic['school'])
+    print(dic['book'])
+main()
