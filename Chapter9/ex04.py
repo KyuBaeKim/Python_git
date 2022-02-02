66093643685fd887d4a9e45d8e7f3905160bc9a5
kyubae <kkyub5118@gmail.com>
2022-02-02 14:12:07 +0900

    리스트 결합, 반복

---
+++ Chapter9/ex04.py
@@ -0,0 +1,12 @@
+# 리스트 결합
+# 리스트 반복
+
+def main():
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [10, 11]
+    listadd = list1 + list2
+    print(listadd)
+
+    listmulti = list2 * 3
+    print(listmulti)
+main()
