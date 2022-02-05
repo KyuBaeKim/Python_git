2973213c560739343d3fef264d0ea1f691412484
kimkyubae <kkyub5118@gmail.com>
2022-02-05 18:26:54 +0900

    VR

---
+++ Chapter16/ex06_vr.py
@@ -0,0 +1,20 @@
+# 가상환경
+# 터미널을 cmd로 설정 한다.
+# conda create --name <가상환경 이름> python=<파이썬 버전>
+# conda create --name pthon_study python=3.7
+
+# 단축키 F1을 눌러서 select interpreter을 가상환경 버전으로 바꾼다.
+
+# <가상환경 실행>
+# conda activate 가상환경 이름
+
+# <가상환경 종료>
+# conda deactivate 가상환경 이름
+
+# <가상환경>
+# 가상환경 리스트 : conda env list
+
+# pip freeze : 다운로드 항목이 나타남
+
+# <가상환경 삭제>
+# conda remove --name <가상환경 이름> --all
